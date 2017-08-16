87dfa5ff2ef7e16d8b9fcefd381328c4d784454e
alundesap <andrew.lunde@sap.com>
2017-08-16 06:45:33 +0000

    adjust remapper

--- tools/remap_approuter.sh
+++ tools/remap_approuter.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 echo XSA Remap AppRouter Starting
 
-js_url="$(xs app XSA_DEV-klqh96msgfjiijux-mta_python-js --urls)"
-python_url="$(xs app XSA_DEV-klqh96msgfjiijux-mta_python-python --urls)"
+js_url="$(xs app python-js --urls)"
+python_url="$(xs app python-python --urls)"
 
-cmd="xs set-env XSA_DEV-klqh96msgfjiijux-mta_python-web destinations '[{\"forwardAuthToken\":true,\"name\":\"js_be\",\"url\":\"$js_url\"},{\"forwardAuthToken\":true,\"name\":\"python_be\",\"url\":\"$python_url\"}]'"
+cmd="xs set-env python-web destinations '[{\"forwardAuthToken\":true,\"name\":\"js_be\",\"url\":\"$js_url\"},{\"forwardAuthToken\":true,\"name\":\"python_be\",\"url\":\"$python_url\"}]'"
 echo $cmd
 eval $cmd
 echo ""
 
-cmd="xs restage XSA_DEV-klqh96msgfjiijux-mta_python-web"
+cmd="xs restage python-web"
 echo $cmd
 eval $cmd
 echo ""
 
-cmd="xs restart XSA_DEV-klqh96msgfjiijux-mta_python-web"
+cmd="xs restart python-web"
 echo $cmd
 eval $cmd
 echo ""
